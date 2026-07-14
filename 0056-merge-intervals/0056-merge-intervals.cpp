ecccc5356cd7c9866b8ae024bcfebb97cd8cf477
Srishti Sharma <srishti1863@gmail.com>
2026-07-15 01:47:22 +0530

    Time: 7 ms (38.45%), Space: 23.9 MB (30.69%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,12 +4,13 @@ public:
         int n=intervals.size();
         sort(intervals.begin(),intervals.end());
         vector<vector<int>>ans;
+        ans.push_back(intervals[0]);
         for(int i=0;i<n;i++){
-            if(ans.empty() || intervals[i][0]>ans.back()[1]){
-                ans.push_back(intervals[i]);
+            if(intervals[i][0]<=ans.back()[1]){
+                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
             }
             else{
-                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+                ans.push_back(intervals[i]);
             }
         }
         return ans;
