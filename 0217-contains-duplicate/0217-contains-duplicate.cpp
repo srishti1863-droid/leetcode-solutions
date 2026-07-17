c183132ba6495fcf151d15ab0299ca7a161550f9
Srishti Sharma <srishti1863@gmail.com>
2026-07-17 18:39:40 +0530

    Time: 71 ms (30.84%), Space: 111.3 MB (16.27%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int>mpp;
+        for(int i=0;i<n;i++){
+            mpp[nums[i]]++;
+            if(mpp[nums[i]]==2) return true;
+        }
+        return false;
+    }
+    
+};
