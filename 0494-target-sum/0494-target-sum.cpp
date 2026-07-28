10a9d271cb43e6dcc12c7eb28301aae373c76ab8
Srishti Sharma <srishti1863@gmail.com>
2026-07-28 23:50:28 +0530

    Time: 12 ms (53.29%), Space: 25.3 MB (29.7%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,17 +1,52 @@
 class Solution {
 public:
-int f(int ind,int target,vector<int>& nums){
-    if(ind==0){
-        if(target==0 && nums[0]==0) return 2;
-        if(target==nums[0] || target==-nums[0]) return 1;
-        return 0;
+    int f(int ind, int target, vector<int>& nums,
+          vector<vector<int>>& dp, int offset) {
+
+        // Target is outside possible range
+        if (target < -offset || target > offset)
+            return 0;
+
+        if (ind == 0) {
+            if (target == 0 && nums[0] == 0)
+                return 2;
+
+            if (target == nums[0] || target == -nums[0])
+                return 1;
+
+            return 0;
+        }
+
+        if (dp[ind][target + offset] != -1)
+            return dp[ind][target + offset];
+
+        int add = f(ind - 1, target - nums[ind],
+                    nums, dp, offset);
+
+        int subtract = f(ind - 1, target + nums[ind],
+                         nums, dp, offset);
+
+        return dp[ind][target + offset] = add + subtract;
     }
-    int add=f(ind-1,target-nums[ind],nums);
-    int subtract=f(ind-1,target+nums[ind],nums);
-    return add+subtract;
-}
+
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n=nums.size();
-        return f(n-1,target,nums);
+        int n = nums.size();
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+
+        if (target > sum || target < -sum)
+            return 0;
+
+        int offset = sum;
+
+        vector<vector<int>> dp(
+            n,
+            vector<int>(2 * sum + 1, -1)
+        );
+
+        return f(n - 1, target, nums, dp, offset);
     }
 };
