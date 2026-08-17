aa6e6c04fb48e9077b0451a4dc55b9e426b1003d
Srishti Sharma <srishti1863@gmail.com>
2026-08-17 14:55:15 +0530

    Time: 3 ms (43.71%), Space: 185.1 MB (68.45%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -10,29 +10,22 @@
  */
 class Solution {
 public:
-int getlength(ListNode* head){
-    int l=0;
-    while(head!=NULL){
-        head=head->next;
-        l++;
-    }
-    return l;
-}
     ListNode* swapNodes(ListNode* head, int k) {
-        int L=getlength(head);
-        int k_1=k;
-        ListNode* Node1=head;
-        while(k_1>1){
-            Node1=Node1->next;
-            k_1--;
+        ListNode* p1=NULL;
+        ListNode* p2=NULL;
+        ListNode* temp=head;
+        while(temp){
+            if(p2!=NULL){
+                p2=p2->next;
+            }
+            k--;
+            if(k==0){
+                p1=temp;
+                p2=head;
+            }
+            temp=temp->next;
         }
-        int k_2=L-k+1;
-        ListNode* Node2=head;
-        while(k_2>1){
-            Node2=Node2->next;
-            k_2--;
-        }
-        swap(Node1->val,Node2->val);
+        swap(p1->val,p2->val);
         return head;
     }
 };
