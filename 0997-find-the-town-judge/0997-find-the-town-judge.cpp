04e5c0ed5d130b83f31fbb0944b2dc0be5d0aa79
Srishti Sharma <srishti1863@gmail.com>
2026-08-22 09:32:28 +0530

    Time: 0 ms (100%), Space: 65 MB (83.89%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int>count(n+1);
+        for(vector<int>& vec:trust){
+            int u=vec[0];
+            int v=vec[1];
+            count[u]--;
+            count[v]++;
+        }
+            for(int i=1;i<=n;i++){
+                if(count[i]==n-1) return i;
+            }
+        
+        return -1;
+    }
+};
